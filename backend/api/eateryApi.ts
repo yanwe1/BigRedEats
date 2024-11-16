610c7368563068e2adbc47a13ae8d71dfdc24823
Sydney Chin <158599418+nysdey@users.noreply.github.com>
2024-11-16 16:37:22 -0500

    test

--- backend/api/eateryApi.ts
+++ backend/api/eateryApi.ts
@@ -5,3 +5,5 @@ export const getEateryDetails = async (eateryId: string) => {
     const doc = await db.collection('eateries').doc(eateryId).get();
     return doc.exists ? doc.data() : null;
 }
+
+console.log('help');
