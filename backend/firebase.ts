5c29dad9fd9de152871403742d73799eadb1bc17
yanwe1 <138923937+yanwe1@users.noreply.github.com>
2024-11-19 09:24:52 -0500

    hi

--- backend/firebase.ts
+++ backend/firebase.ts
@@ -2,12 +2,14 @@
 import { initializeApp, applicationDefault, cert } from "firebase-admin/app";
 import { getFirestore } from 'firebase-admin/firestore';
 
-// import serviceAccount from "./serviceAccountKey.json";
+import serviceAccount from "./service_account.json";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyCnDk8m_Css_cooyDN8QxoycQI3_zLh0WQ", 
   authDomain: "final-project-74749.firebaseapp.com",
@@ -17,9 +19,13 @@ const firebaseConfig = {
   appId: "1:1008006262930:web:a3f10707cedafb9aa1f64b"
 };
 
-
+// const app = admin.initializeApp({
+//   credential: cert(serviceAccount as ServiceAccount)
+//   databaseURL: 'https://final-project-74749.firebaseio.com'
+// });
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 
 // export firebase instances for easy access
 const db = getFirestore(app);
