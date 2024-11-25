f526fc90bb9540dba0e62a0651c1ece4c06b7196
Sydney Chin <158599418+nysdey@users.noreply.github.com>
2024-11-25 11:24:47 -0500

    did change 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as dotenv from "dotenv";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env": process.env, // Polyfill process.env
+  },
+});
